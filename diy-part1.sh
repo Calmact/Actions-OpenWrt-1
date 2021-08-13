467cec33778a9f5163b4e66fb054833e27b05e05
Calmact <Calmact@gmail.com>
2021-08-13 08:57:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/MrH723/openwrt-packages.git package/openwrt-packages
 sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
